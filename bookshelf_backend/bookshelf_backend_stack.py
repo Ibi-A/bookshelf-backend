ee0e877fbc271e71b9cb00d4f7fb2b26194cc623
micamath <michau.mathilde@gmail.com>
2020-05-15 16:27:49 +0200

    basic architecture to test graphql in lambda

--- bookshelf_backend/bookshelf_backend_stack.py
+++ bookshelf_backend/bookshelf_backend_stack.py
@@ -1,4 +1,6 @@
 from aws_cdk import core
+from aws_cdk.aws_dynamodb import Table, Attribute, AttributeType
+from aws_cdk.aws_lambda import Function, Runtime, Code
 
 
 class BookshelfBackendStack(core.Stack):
@@ -6,4 +8,23 @@ class BookshelfBackendStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        dyn_books = Table(self, 'dyn_books',
+                          table_name="books",
+                          partition_key=Attribute(
+                              name="isbn",
+                              type=AttributeType.STRING))
+
+        dyn_users = Table(self, 'dyn_users',
+                          table_name="users",
+                          partition_key=Attribute(
+                              name="user_id",
+                              type=AttributeType.STRING))
+
+        lba_graphql = Function(self, "lba_graphql",
+                               function_name="graphql",
+                               code=Code.asset("./source_code/lba_graphql"),
+                               handler="lba_graphql.lambda_handler",
+                               runtime=Runtime.PYTHON_3_7)
+
+        dyn_books.grant_full_access(lba_graphql)
+        dyn_users.grant_full_access(lba_graphql)
